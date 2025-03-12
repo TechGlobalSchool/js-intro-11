4490713c05f7eb4f93373341c8ccc75d3fdfc87f
TechGlobalSchool <tgschool01@gmail.com>
2025-03-11 21:05:33 -0500

    added es6

--- js-miscellaneous/11-errors.js
+++ js-miscellaneous/11-errors.js
@@ -5,6 +5,6 @@ pi = 10; // TypeError: Assignment to constant variable.
 console.log(address); // ReferenceError: address is not defined
 
 // Syntax Error
-console.log('Hello'; // SyntaxError: missing ) after argument list
+//console.log('Hello'; // SyntaxError: missing ) after argument list
 
 // 'hello.toLowerCase(); // SyntaxError: Invalid or unexpected token
---
+++ test.js
@@ -0,0 +1,6 @@
+function printHi() {
+  console.log('HEllo');
+  printHi();
+}
+
+printHi();
