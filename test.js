e29daf0324f222e25dd31d98cc20f8461d25bf55
TechGlobalSchool <tgschool01@gmail.com>
2025-03-15 13:59:51 -0500

    added prototypes and es6 classes

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
-function printHi() {
-  console.log('HEllo');
-  printHi();
-}
-
-printHi();
+// 2 ways 
+// CommonJS (module.exports require) - used with NodeJS
+// ES6 Modules (import export) - are not directly supported with NodeJS but browsers
+// Cypress Playwright -> They use both (both are supported)
+// 
