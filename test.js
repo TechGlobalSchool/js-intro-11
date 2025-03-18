7cb305100e7160a2b25f19e011c0218ff75b640d
TechGlobalSchool <tgschool01@gmail.com>
2025-03-17 21:05:51 -0500

    added more OOP

--- test.js
+++
@@ -1,5 +0,0 @@
-// 2 ways 
-// CommonJS (module.exports require) - used with NodeJS
-// ES6 Modules (import export) - are not directly supported with NodeJS but browsers
-// Cypress Playwright -> They use both (both are supported)
-// 
